aa614f7d2398047f38e45485d3855d5e8d9a5e9f
kauan <kauangfx@hotmail.com>
2022-04-27 21:06:40 -0300

    Subindo 27/04

---
+++ JS/Aula 02 JS - Console/Exercicio/main.js
@@ -0,0 +1,11 @@
+console.log("Saiba qual o seu IMC!");
+
+var nome = "Kauan";
+var peso = 78;
+var altura = 1.80;
+
+var valor = peso/(altura*altura);
+
+
+const resultado = `Olá ${nome} seu peso é ${peso} e sua altura é ${altura}m e seu IMC é ${valor}`
+console.log(resultado)
