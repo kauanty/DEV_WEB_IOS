abe463ee5b340bc7f0055f02aacb27fba35896f2
kauanty <kauangfx@hotmail.com>
2022-06-06 21:50:25 -0300

    up

---
+++ React/05_Agenda/tema_05_agenda/src/App.js
@@ -0,0 +1,10 @@
+import Header from './Components/Header.jsx';
+
+function App() {
+  return (
+    <div className="container">
+      <Header tittle="tarefas" />
+    </div>
+  );
+}
+export default App; 
