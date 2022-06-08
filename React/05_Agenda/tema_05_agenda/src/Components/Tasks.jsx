0f37fc15dd517de50e94346803520931a9476c6a
kauanty <kauangfx@hotmail.com>
2022-06-08 20:42:42 -0300

    Aula 14 JS

--- React/05_Agenda/tema_05_agenda/src/Components/Tasks.jsx
+++ React/05_Agenda/tema_05_agenda/src/Components/Tasks.jsx
@@ -1,9 +1,14 @@
 import TaskItem from './TaskItem';
-const Tasks = ({ tasks }) => {
+const Tasks = ({ tasks, onDelete, onToggle }) => {
     return (
         <>
             {tasks.map((task) => (
-                <TaskItem key={task.id} task={task} />
+                <TaskItem
+                    key={task.id}
+                    task={task}
+                    onDelete={onDelete}
+                    onToggle={onToggle}
+                />
             ))}
         </>
     );
