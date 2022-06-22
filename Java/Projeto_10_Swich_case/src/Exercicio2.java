204da517b76d68cb411d52ed11e1d740d21c0b0f
kauanty <kauangfx@hotmail.com>
2022-06-22 18:43:07 -0300

    22

--- Java/Projeto_10_Swich_case/src/Exercicio2.java
+++ Java/Projeto_10_Swich_case/src/Exercicio2.java
@@ -4,23 +4,23 @@ public class Exercicio2 {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int num1, num2, num3;
+        double num1, num2, num3;
         System.out.println("    = = = = = Escolha o primeiro número = = = = =    ");
-        num1 = input.nextInt();
+        num1 = input.nextDouble();
         System.out.println("    = = = = = Escolha o segundo número = = = = =    ");
-        num2 = input.nextInt();
+        num2 = input.nextDouble();
         System.out.println("    = = = = = Escolha o terceiro número = = = = =    ");
-        num3 = input.nextInt();
+        num3 = input.nextDouble();
 
         if (num1 == num2 & num1 == num3) {
-            System.out.println("Os números sao iguais");
+            System.out.println("Os números são iguais");
         } else {
             if (num1 > num2 & num1 > num2) {
-                System.out.println("O " + num1 + "  é o maior número");
+                System.out.println( + num1 + "  é o maior número");
             } else if (num2 > num1 & num2 > num3) {
-                System.out.println("O " + num2 + "  é o maior número");
+                System.out.println( + num2 + "  é o maior número");
             } else {
-                System.out.println("O " + num3 + "  é o maior número");
+                System.out.println( + num3 + "  é o maior número");
             }
 
             input.close();
